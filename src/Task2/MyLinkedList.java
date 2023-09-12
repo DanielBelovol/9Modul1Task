36320030b2a28d598fe4b37110642f5af4f4b88e
Daniel <danielbelovol08@gmail.com>
2023-09-12 17:46:40 +0300

    Finish 2Task

--- src/Task2/MyLinkedList.java
+++ src/Task2/MyLinkedList.java
@@ -5,7 +5,17 @@ import java.util.Objects;
 public class MyLinkedList <T>{
     private Node<T> head;
     private Node<T> tail;
-    public int size;
+    private int size;
+
+    static class Node<T>{
+        T value;
+        Node<T> next;
+        Node<T> previous;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
 
     public void add(T value){
         Node<T> node = new Node<>(value);
@@ -32,15 +42,39 @@ public class MyLinkedList <T>{
         return  current.value;
     }
 
+    public int size(){
+        return size;
+    }
 
-
-    static class Node<T>{
-        T value;
-        Node<T> next;
-        Node<T> previous;
-
-        public Node(T value) {
-            this.value = value;
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
         }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        if (current.previous != null) {
+            current.previous.next = current.next;
+        } else {
+            head = current.next;
+            if (head != null) {
+                head.previous = null;
+            }
+        }
+        if (current.next != null) {
+            current.next.previous = current.previous;
+        } else {
+            tail = current.previous;
+            if (tail != null) {
+                tail.next = null;
+            }
+        }
+        size--;
+    }
+    public void clear(){
+        head = null;
+        tail = null;
+        size = 0;
     }
 }
--- src/Task2/MyLinkedListTest.java
+++ src/Task2/MyLinkedListTest.java
@@ -1,6 +1,21 @@
 package Task2;
 
+import java.util.LinkedList;
+
 public class MyLinkedListTest {
     public static void main(String[] args) {
+        MyLinkedList<String> linkedList = new MyLinkedList<>();
+
+        linkedList.add("first");
+        linkedList.add("second");
+        linkedList.add("thirt");
+        linkedList.add("fourth");
+        linkedList.add("five");
+
+        linkedList.remove(0);
+
+        for (int i = 0; i < linkedList.size(); i++) {
+            System.out.println(linkedList.get(i));
+        }
     }
 }

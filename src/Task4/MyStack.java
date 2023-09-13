1906609bc5146e62029e3754e25d3526612f8b8c
Daniel <danielbelovol08@gmail.com>
2023-09-13 20:34:56 +0300

    Finish 4Task

--- src/Task4/MyStack.java
+++ src/Task4/MyStack.java
@@ -1,4 +1,87 @@
 package Task4;
 
-public class MyStack {
+import Task2.MyLinkedList;
+import Task3.MyQueue;
+
+public class MyStack<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+    static class Node<T>{
+        T value;
+        private Node<T> next;
+        private Node<T> previous;
+
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public void push(T value){
+        Node<T> node = new Node<>(value);
+
+        if(head==null){
+            head = node;
+            tail = node;
+        }else{
+            tail.next = node;
+            node.previous = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    public void remove(int index){
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        if (current.previous != null) {
+            current.previous.next = current.next;
+        } else {
+            head = current.next;
+            if (head != null) {
+                head.previous = null;
+            }
+        }
+        if (current.next != null) {
+            current.next.previous = current.previous;
+        } else {
+            tail = current.previous;
+            if (tail != null) {
+                tail.next = null;
+            }
+        }
+        size--;
+    }
+
+    public void clear(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public T peek(){
+        return tail.value;
+    }
+
+    public T pop(){
+        Node<T> current = tail;
+        try{
+            return(T) tail.value;
+        }finally {
+            tail = current.previous;
+            if (tail != null) {
+                tail.next = null;
+            }
+        }
+    }
 }
--- src/Task4/MyStackTest.java
+++ src/Task4/MyStackTest.java
@@ -1,4 +1,14 @@
 package Task4;
 
 public class MyStackTest {
+    public static void main(String[] args) {
+        MyStack<String> myStack = new MyStack<>();
+        myStack.push("Hello1");
+        myStack.push("Hello2");
+        myStack.push("Hello3");
+
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+    }
+
 }
